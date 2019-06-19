b306d41aa524a8f2db80a5513753a568631fea1f
Shabalin, Piotr (BeliyKit) <piotr.shabalin@it-wk.ru>
2019-06-19 16:09:28 +0300

    Initial commit

---
+++ src/main/java/com/example/sweater/Application.java
@@ -0,0 +1,13 @@
+package com.example.sweater;//  Created by piotr.shabalin on 19.06.2019 15:55
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
